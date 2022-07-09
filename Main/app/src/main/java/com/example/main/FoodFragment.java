08fd315fdfa90ddc97302df7c96706677be49aff
조윤서 <younseu2@gmail.com>
2022-07-09 17:59:43 +0900

    edit android studio

--- Main/app/src/main/java/com/example/main/FoodFragment.java
+++ Main/app/src/main/java/com/example/main/FoodFragment.java
@@ -1,15 +1,66 @@
 package com.example.main;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.File;
+
 public class FoodFragment extends Fragment {
+
+    Bitmap bitmap;
+
+    public Button get_btn_picture;
+    public ImageView imgV;
+    public File file;
+
+
+    private static final int REQUEST_IMAGE_CODE = 101;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_food, container, false); //attachToRoot: false 안함.
+
+        View view = inflater.inflate(R.layout.fragment_food, container, false);
+
+        get_btn_picture = view.findViewById(R.id.btn_picture);
+        imgV = view.findViewById(R.id.imageView);
+
+        get_btn_picture.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultPicture.launch(intent);
+            }
+        });
+
+        return view; //attachToRoot: false 안함.
     }
+
+    ActivityResultLauncher<Intent> activityResultPicture = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    //결과 ok, 데이터 null 아니라면
+                    if(result.getResultCode() == Activity.RESULT_OK && result.getData() != null){
+                        Bundle extras = result.getData().getExtras();
+                        bitmap = (Bitmap) extras.get("data");
+
+                        imgV.setImageBitmap(bitmap);
+                    }
+                }
+            }
+    );
 }
--- Main/app/src/main/java/com/example/main/MainActivity.java
+++ Main/app/src/main/java/com/example/main/MainActivity.java
@@ -1,25 +1,76 @@
 package com.example.main;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_PERMISSION = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkPermission();
+
         NavigationBarView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnItemSelectedListener(navListener);
 
+
+    }
+
+
+    //권한 확인
+    public void checkPermission() {
+        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int permissionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        //권한이 없으면 권한 요청
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED
+                || permissionRead != PackageManager.PERMISSION_GRANTED
+                || permissionWrite != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "이 앱을 실행하기 위해 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                // 권한이 취소되면 result 배열은 비어있다.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "권한 확인", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+                    finish(); //권한이 없으면 앱 종료
+                }
+            }
+        }
     }
 
     private NavigationBarView.OnItemSelectedListener navListener =
@@ -43,5 +94,4 @@ public class MainActivity extends AppCompatActivity {
 
                 return true;
             };
-
 }
