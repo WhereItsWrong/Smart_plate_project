13e7af0af4f98294c4353d74168e1feebbe96997
박승호 <psh9239684@gmail.com>
2022-08-02 16:59:24 +0900

    앱 지연 해결(권한 코드 관련 지연 수정)

--- Main/app/src/main/java/com/example/main/PermissionSupport.java
+++ Main/app/src/main/java/com/example/main/PermissionSupport.java
@@ -22,7 +22,6 @@ public class PermissionSupport {
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.CAMERA,
             Manifest.permission.READ_EXTERNAL_STORAGE,
