902212bd2dc647018ec27cf4fdd8edb4dc703bd8
김재민 <woals4410@naver.com>
2022-09-22 17:50:15 +0900

    가중치파일 업데이트 및 torchscript_lite 파일로 변환 img_cut.py 수정 그릇 영상 업로드

--- yolov5/img_cut.py
+++ yolov5/img_cut.py
@@ -1,7 +1,7 @@
 import cv2
 from sympy import true
 
-video_path = "yolov5/data/videos/Funny2SecondVideo.mp4"
+video_path = "data/videos/black_PLATE1.mp4"
 vidcap = cv2.VideoCapture(video_path)
 
 count = 1
@@ -13,9 +13,9 @@ while success:
   if success:
     image90 = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
     image270 = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    cv2.imwrite("yolov5/tmp/%d.jpg" % count, image)
-    cv2.imwrite("yolov5/tmp/Right%d.jpg" % count, image90)
-    cv2.imwrite("yolov5/tmp/Left%d.jpg" % count, image270)
+    cv2.imwrite("tmp/%d.jpg" % count, image)
+    cv2.imwrite("tmp/Right%d.jpg" % count, image90)
+    cv2.imwrite("tmp/Left%d.jpg" % count, image270)
     print("%dth image store" % count)
   
   if cv2.waitKey(10) == 27:                    
